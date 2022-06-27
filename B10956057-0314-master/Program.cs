6398ba9f4d67985a62a2e5d3c456da2b7ae1e06c
B10956057 <84553528+B10956057@users.noreply.github.com>
2022-06-27 22:13:11 +0800

    Add files via upload

---
+++ B10956057-0314-master/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10956057_0314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個數:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                if (2<=a&&a<=5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (6 <= a && a <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (a > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else
+                {
+                    Console.WriteLine("Weird");
+                }
+                    
+            }
+            
+        }
+    }
+}
